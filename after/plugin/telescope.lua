a5fc88266f5964d87a475cc24d0b22dd3b9b7ee4
linguini1 <matteo.golin@gmail.com>
2023-05-11 13:47:24 -0400

    Telescope now ignores venv, node_modules, .git and .idea directories.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,17 @@
 local builtin = require("telescope.builtin")
+local telescope = require("telescope")
+
+telescope.setup{
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            "venv",
+            ".git",
+            ".idea",
+        }
+    }
+}
+
 vim.keymap.set('n', '<C-f>', builtin.find_files, {}) -- Find files
 vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- Find git files
 
