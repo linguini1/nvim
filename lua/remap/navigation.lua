8aaeef2352832290b3fba04d8f91fab7322ec849
Matteo Golin <matteo.golin@gmail.com>
2023-05-12 14:29:41 -0400

    Added descriptions to navigation keywords.

--- lua/remap/navigation.lua
+++ lua/remap/navigation.lua
@@ -1,5 +1,5 @@
 -- Quick window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Jump to left adjacent window." })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Jump to bottom adjacent window." })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Jump to top adjacent window." })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Jump to right adjacent window." })
