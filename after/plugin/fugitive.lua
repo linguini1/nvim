e68961fe7c20bab6a5225ea35d091e2f0beb53bc
Matteo Golin <matteo.golin@gmail.com>
2023-05-11 22:18:09 -0400

    Added some leader key shortcuts for finding files/symbols using Telescope, and some shortcuts for my commonly used git commands.

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,17 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Gitt)
+-- Adding
+vim.keymap.set("n", "<leader>ga", ":Git add", {desc = "Set up git add."})
+vim.keymap.set("n", "<leader>gaa", ":Git add .<CR>", {desc = "Add all files to git."})
+
+-- Restoring
+vim.keymap.set("n", "<leader>gr", ":Git restore --staged", {desc = "Set up git restore staged files."})
+vim.keymap.set("n", "<leader>gra", ":Git restore --staged .<CR>", {desc = "Restore all staged files."})
+
+-- Committing
+vim.keymap.set("n", "<leader>gc", ":Git commit -m \"\"<left>", {desc = "Set up git commit."})
+vim.keymap.set("n", "<leader>gca", ":Git commit -a -m \"\"<left>", {desc = "Sets up a git commit which automatically adds modified files."})
+vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {desc = "Check git status."})
+
+-- Pushing/pulling
+vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {desc = "Push changes to the git repo."})
+vim.keymap.set("n", "<leader>gf", ":Git fetch<CR>", {desc = "Fetch changes from the git repo."})
+vim.keymap.set("n", "<leader>gpu", ":Git pull<CR>", {desc = "Pull changes from the git repo."})
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require("telescope.builtin")
 local telescope = require("telescope")
 
-telescope.setup{
+telescope.setup {
     defaults = {
         file_ignore_patterns = {
             "node_modules",
@@ -12,10 +12,10 @@ telescope.setup{
     }
 }
 
-vim.keymap.set('n', '<C-f>', builtin.find_files, {}) -- Find files
-vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- Find git files
-
--- Search strings
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("grep > ") });
-end)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files using Telescope." })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Find git file using Telescope." })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Find keymaps using Telescope." })
+vim.keymap.set('n', '<leader>fc', builtin.git_commits, { desc = "Find git commits using Telescope." })
+vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = "Find git branches using Telescope." })
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "Find symbols using Telescope." })
+vim.keymap.set('n', '<leader>fp', builtin.live_grep, { desc = "Find token using live grep in Telescope." })
