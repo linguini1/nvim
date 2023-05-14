0a2a1ade5729f8737469b25877f693d39439ec9c
Matteo Golin <matteo.golin@gmail.com>
2023-05-14 19:11:58 -0400

    Added lualine plugin.

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require("linguini")
-require("remap")
 require("set")
 require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/luasnippets/"})
--- lua/linguini/packer.lua
+++ lua/linguini/packer.lua
@@ -1,24 +1,16 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
-
-    use { "ellisonleao/gruvbox.nvim" }
-
+    use { "ellisonleao/gruvbox.nvim" } -- Colour
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
-
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
@@ -38,30 +30,32 @@ return require('packer').startup(function(use)
             { 'saadparwaiz1/cmp_luasnip' }, -- Shows lua snippets in completion menu
             { 'hrsh7th/cmp-path' },         -- Shows paths in completion menu
             { 'hrsh7th/cmp-nvim-lua' },     -- Shows nvim api in completion menu
-            { 'hrsh7th/nvim-cmp' },         -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-            { 'L3MON4D3/LuaSnip' },         -- Required
+            { 'hrsh7th/nvim-cmp' },         -- Completion
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Completion + LSP integration
+            { 'L3MON4D3/LuaSnip' },         -- Snippets
         }
     }
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
+            'nvim-tree/nvim-web-devicons', -- Pretty icons
         },
         config = function()
             require("nvim-tree").setup {}
         end
     }
-
     use {
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
-    use({
+    use {
         "jose-elias-alvarez/null-ls.nvim",
         config = function()
             require("null-ls").setup()
         end,
         requires = { "nvim-lua/plenary.nvim" },
-    })
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+    }
 end)
