f9781e6dd3c55b32f84ee5a67e305d9f08716562
Matteo Golin <matteo.golin@gmail.com>
2023-05-17 21:14:53 -0400

    Added a keybinding for deleting word in front of cursor in insert mode.

--- lua/remap/editing.lua
+++ lua/remap/editing.lua
@@ -5,3 +5,6 @@ vim.keymap.set("n", "<C-s>", "<Cmd>update<CR>", {desc = "Save the current buffer
 -- Move highlighted block up and down + autoindent
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move line(s) downward with correct indentation."})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move line(s) upward with correct indentation."})
+
+-- Deleting
+vim.keymap.set("i", "<C-b>", "<Esc><right>dwgi", {desc = "Delete word in front of cursor."})
