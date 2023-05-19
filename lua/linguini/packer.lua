0fd8e61792555f5767cd16691ba54198de1e45ff
linguini1 <matteo.golin@gmail.com>
2023-05-19 13:51:09 -0400

    Cleaned up packer file a little.

--- lua/linguini/packer.lua
+++ lua/linguini/packer.lua
@@ -1,9 +1,9 @@
 -- Packer bootstrapping
-local ensure_packer = function ()
+local ensure_packer = function()
     local fn = vim.fn
-    local install_path = fn.stdpath("data").."site/pack/packer/start/packer.nvim"
+    local install_path = fn.stdpath("data") .. "site/pack/packer/start/packer.nvim"
     if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
         vim.cmd [[packadd packer.nvim]]
         return true
     end
@@ -18,10 +18,10 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
-    use { "ellisonleao/gruvbox.nvim" } -- Colour
+    use "ellisonleao/gruvbox.nvim"  -- Colour scheme
+    use 'mbbill/undotree'
+    use 'tpope/vim-fugitive'
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
@@ -63,9 +63,7 @@ return require('packer').startup(function(use)
         "jose-elias-alvarez/null-ls.nvim",
         requires = { "nvim-lua/plenary.nvim" },
     }
-    use {
-        'nvim-lualine/lualine.nvim',
-    }
+    use 'nvim-lualine/lualine.nvim'
 
     if packer_bootsrap then
         require("packer").sync()
