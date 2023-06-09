dbff4ef978b8d4ed6be9e9ab58855b0bc72130c4
linguini1 <matteo.golin@gmail.com>
2023-06-08 21:49:40 -0400

    Added Flake8 diagnostics.

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -1,11 +1,13 @@
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
     formatting.black.with({ extra_args = { "--line-length=120" } }),
     formatting.prettierd.with({
         filetypes = { "html", "css", "json", "markdown", }
     }),
+    diagnostics.flake8.with({extra_args = {"--max-line-length", "120"}})
 }
 
 null_ls.setup({
