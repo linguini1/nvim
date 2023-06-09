66a6ec37bb193b8b757b3286b9bc3beacb00118c
linguini1 <matteo.golin@gmail.com>
2023-06-09 12:25:31 -0400

    Added fold toggle with Enter key

--- lua/remap/navigation.lua
+++ lua/remap/navigation.lua
@@ -5,13 +5,4 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Jump to top adjacent window." }
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Jump to right adjacent window." })
 
 -- Code folds
-local function toggle_fold()
-   local lnum, col = vim.api.nvim_win_get_cursor(0)
-   local cursor_on_fold = vim.fn.foldclosed(lnum)
-   if cursor_on_fold == -1 then
-       vim.fn.expand("zc")
-   else
-       vim.fn.expand("zo")
-   end
-end
-vim.keymap.set("n", "<CR>", toggle_fold, {desc = "Toggle fold open/close."})
+vim.keymap.set("n", "<CR>", "za", {desc = "Toggle fold open/close."})
