52060740dd984e31d1ada5ced82efa1512e6a3ed
Matteo Golin <matteo.golin@gmail.com>
2023-06-11 13:33:11 -0400

    Made filepath in lualine relative to root directory.

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,8 @@
-require("lualine").setup {
-    options = {
-        theme = "gruvbox"
-    }
-}
+require("lualine").setup({
+	options = {
+		theme = "auto",
+	},
+	sections = {
+		lualine_c = { { "filename", path = 1 } },
+	},
+})
