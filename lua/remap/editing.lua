0c5acc737fa9708a7037f02c342d80e378c85f23
Matteo Golin <matteo.golin@gmail.com>
2023-06-12 21:12:52 -0400

    Added quick command for updating buffers.

--- lua/remap/editing.lua
+++ lua/remap/editing.lua
@@ -1,13 +1,18 @@
 -- Saving
-vim.keymap.set("i", "<C-s>", "<Esc><Cmd>update<CR>gi", {desc = "Save the current buffer and return the cursor to its place."})
-vim.keymap.set("n", "<C-s>", "<Cmd>update<CR>", {desc = "Save the current buffer."})
+vim.keymap.set(
+	"i",
+	"<C-s>",
+	"<Esc><Cmd>update<CR>gi",
+	{ desc = "Save the current buffer and return the cursor to its place." }
+)
+vim.keymap.set("n", "<leader>u", "<Cmd>update<CR>", { desc = "update current buffer." })
 
 -- Move highlighted block up and down + autoindent
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move line(s) downward with correct indentation."})
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move line(s) upward with correct indentation."})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line(s) downward with correct indentation." })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line(s) upward with correct indentation." })
 
 -- Deleting
-vim.keymap.set("i", "<C-b>", "<Esc><right>dwgi", {desc = "Delete word in front of cursor."})
+vim.keymap.set("i", "<C-b>", "<Esc><right>dwgi", { desc = "Delete word in front of cursor." })
 
 -- Formatting
-vim.keymap.set("n", "gq", "<Cmd>lua vim.lsp.buf.format({async = true})<CR>", {desc = "Format the current buffer."})
+vim.keymap.set("n", "gq", "<Cmd>lua vim.lsp.buf.format({async = true})<CR>", { desc = "Format the current buffer." })
