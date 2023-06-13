054ca9fd2057bcfdd5a0dea85cd7f4095843e020
Matteo Golin <matteo.golin@gmail.com>
2023-06-12 21:49:09 -0400

    Removed enter to fold because of conflict with reference navigation.

--- lua/remap/convenience.lua
+++ lua/remap/convenience.lua
@@ -1,2 +1,2 @@
 vim.keymap.set("n", "<C-R>", "<Cmd>source $MYVIMRC<CR>", { desc = "Reload nvim config." })
-
+>
--- lua/remap/navigation.lua
+++ lua/remap/navigation.lua
@@ -3,6 +3,3 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Jump to left adjacent window." 
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Jump to bottom adjacent window." })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Jump to top adjacent window." })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Jump to right adjacent window." })
-
--- Code folds
-vim.keymap.set("n", "<CR>", "za", {desc = "Toggle fold open/close."})
