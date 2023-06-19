7bed52118ad577ef4fd5ff765ae8b0d13dcc5ecc
Matteo Golin <matteo.golin@gmail.com>
2023-06-19 19:16:09 -0400

    Added some more settings for clang-format but still have not fixed the jitter issue without having a .clang-format file in the working directory of a project.

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -3,20 +3,20 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
-    formatting.black.with({ extra_args = { "--line-length=120" } }),
-    formatting.prettierd.with({
-        filetypes = { "html", "css", "json", "markdown", "javascriptreact", "javascript" },
-    }),
-    formatting.stylua,
-    formatting.clang_format.with({
-        filetypes = { "c", "c++" },
-        extra_args = { "-style={IndentWidth: 4}" }
-    }),
-    diagnostics.flake8.with({ extra_args = { "--max-line-length", "120", "--ignore=E203" } }),
-    diagnostics.eslint,
+	formatting.black.with({ extra_args = { "--line-length=120" } }),
+	formatting.prettierd.with({
+		filetypes = { "html", "css", "json", "markdown", "javascriptreact", "javascript" },
+	}),
+	formatting.stylua,
+	formatting.clang_format.with({
+		filetypes = { "c", "c++" },
+		extra_args = { "-style={IndentWidth: 4, ColumnLimit: 120}" },
+	}),
+	diagnostics.flake8.with({ extra_args = { "--max-line-length", "120", "--ignore=E203" } }),
+	diagnostics.eslint,
 }
 
 null_ls.setup({
-    debug = true,
-    sources = sources,
+	debug = true,
+	sources = sources,
 })
