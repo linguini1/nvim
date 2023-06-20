34d5f472d41a8d4dd938579ea60c643846353fd9
linguini1 <matteo.golin@gmail.com>
2023-06-20 13:17:24 -0400

    Added snippet for journal headings.

---
+++ luasnippets/text.lua
@@ -0,0 +1,9 @@
+-- Regular snippets
+return {
+
+    },
+
+    -- Autosnippets
+    {
+        s({ trig = "!journot", dscr = ".txt journal entry header." }, { t("=== "), i(1, "title"), t(" ===") })
+    }
