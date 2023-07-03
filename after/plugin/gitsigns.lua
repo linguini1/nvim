f2c10144b8c6dcf52feb1d57d823e49805b7fa2d
Matteo Golin <matteo.golin@gmail.com>
2023-07-03 17:06:59 -0400

    Fixed compiled packer tracking in gitignore and removed word diff because it made cursor rendering ugly.

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,6 +1,4 @@
 local gs = require("gitsigns")
-gs.setup({
-    word_diff = true,
-})
+gs.setup()
 
 vim.keymap.set("n", "<leader>gbl", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Git blame on current line." })
