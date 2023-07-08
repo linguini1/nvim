0543cf2c46afea994b1cc47dde4f0b6bb1838cc9
Matteo Golin <matteo.golin@gmail.com>
2023-07-08 11:19:32 -0400

    Show branches but not remotes.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,22 +1,24 @@
 local builtin = require("telescope.builtin")
 local telescope = require("telescope")
 
-telescope.setup {
+telescope.setup({
     defaults = {
         file_ignore_patterns = {
             "^node_modules/",
             "^venv/",
             "^./.git/",
             ".idea",
-        }
-    }
-}
+        },
+    },
+})
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files using Telescope." })
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Find git file using Telescope." })
-vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Find keymaps using Telescope." })
-vim.keymap.set('n', '<leader>fc', builtin.git_commits, { desc = "Find git commits using Telescope." })
-vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = "Find git branches using Telescope." })
-vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "Find symbols using Telescope." })
-vim.keymap.set('n', '<leader>fp', builtin.live_grep, { desc = "Find token using live grep in Telescope." })
-vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = "Show references in a Telescope window." })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files using Telescope." })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find git file using Telescope." })
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find keymaps using Telescope." })
+vim.keymap.set("n", "<leader>fc", builtin.git_commits, { desc = "Find git commits using Telescope." })
+vim.keymap.set("n", "<leader>fb", function()
+    return builtin.git_branches({ show_remote_tracking_branches = false })
+end, { desc = "Find git branches using Telescope." })
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find symbols using Telescope." })
+vim.keymap.set("n", "<leader>fp", builtin.live_grep, { desc = "Find token using live grep in Telescope." })
+vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Show references in a Telescope window." })
