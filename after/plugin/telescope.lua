8a5b2ca8bfb700c610b49576cccdc2dbc228e210
Matteo Golin <matteo.golin@gmail.com>
2023-07-08 14:20:07 -0400

    Added spell suggest replacement.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -14,10 +14,11 @@ telescope.setup({
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files using Telescope." })
 vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find git file using Telescope." })
-vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find keymaps using Telescope." })
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find key maps using Telescope." })
 vim.keymap.set("n", "<leader>fc", builtin.git_commits, { desc = "Find git commits using Telescope." })
 vim.keymap.set("n", "<leader>fb", function()
     return builtin.git_branches({ show_remote_tracking_branches = false })
 end, { desc = "Find git branches using Telescope." })
 vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find symbols using Telescope." })
 vim.keymap.set("n", "<leader>fp", builtin.live_grep, { desc = "Find phrase using live grep in Telescope." })
+vim.keymap.set("n", "z=", builtin.spell_suggest, { desc = "Show spelling suggestions for word under cursor." })
