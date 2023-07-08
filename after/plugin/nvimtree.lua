05db6841ce451aef2c50127930c4cc0ec4ae0e0a
Matteo Golin <matteo.golin@gmail.com>
2023-07-08 10:50:59 -0400

    Cut down on nvimtree commands and added descriptions.

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,16 +1,13 @@
 -- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-    hijack_cursor = true, -- Cursor on first letter of fname during nav
+    hijack_cursor = true, -- Cursor on first letter of file name during nav
 })
 
 -- Tree commands
-vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader>tff", ":NvimTreeFindFile<CR>")
-vim.keymap.set("n", "<leader>tc", ":NvimTreeCollapse<CR>")
-vim.keymap.set("n", "<leader>tf", ":NvimTreeFocus<CR>")
-vim.keymap.set("n", "<leader>tr", ":NvimTreeRefresh<CR>")
+vim.keymap.set("n", "<leader>tt", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree." })
+vim.keymap.set("n", "<leader>tf", "<Cmd>NvimTreeFocus<CR>", { desc = "Bring cursor to file tree." })
+vim.keymap.set("n", "<leader>tr", "<Cmd>NvimTreeRefresh<CR>", { desc = "Refresh the file tree." })
