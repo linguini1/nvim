788cbe66fee616db400bebe34aec973eb2fbe5c4
Matteo Golin <matteo.golin@gmail.com>
2023-07-16 18:37:37 -0400

    Configured clangd to warn all and configured lua-ls via lspconfig instead.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,6 +2,7 @@
 local lsp = require("lsp-zero").preset("recommended")
 local cmp = require("cmp")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local lspconfig = require("lspconfig")
 
 -- Luasnip
 local ls = require("luasnip")
@@ -9,96 +10,99 @@ local ls = require("luasnip")
 -- Luasnippets
 require("luasnip.loaders.from_lua").load()
 ls.setup({
-    history = true,
-    update_events = { "TextChanged", "TextChangedI" },
-    enable_autosnippets = true,
+	history = true,
+	update_events = { "TextChanged", "TextChangedI" },
+	enable_autosnippets = true,
 })
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
-    vim.keymap.set(
-        "n",
-        "gr",
-        "<Cmd>Telescope lsp_references<CR>",
-        { buffer = true, desc = "Show references in a Telescope window." }
-    )
+	lsp.default_keymaps({ buffer = bufnr })
+	vim.keymap.set(
+		"n",
+		"gr",
+		"<Cmd>Telescope lsp_references<CR>",
+		{ buffer = true, desc = "Show references in a Telescope window." }
+	)
 end)
 
 -- Language servers
 lsp.ensure_installed({
-    "eslint",
-    "pyright",
-    "emmet_ls",
-    "cssls",
-    "lua_ls",
-    "cmake",
-    "clangd",
+	"eslint",
+	"pyright",
+	"emmet_ls",
+	"cssls",
+	"lua_ls",
+	"cmake",
+	"clangd",
 })
-lsp.configure("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-        },
-    },
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
+lspconfig.clangd.setup({
+	arguments = { "-Wall" },
 })
 
 -- Completion setup
 cmp.setup({
-    sources = {
-        { name = "nvim_lua" },
-        { name = "nvim_lsp" },
-        { name = "path" },
-        { name = "luasnip", option = { show_autosnippets = true } },
-    },
-    mapping = {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif ls.expand_or_jumpable() then
-                ls.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }), -- Tab autocomplete
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif ls.jumpable(-1) then
-                ls.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Enter to complete
-        ["<Up>"] = cmp.mapping.abort(),               -- No up and down selection
-        ["<Down>"] = cmp.mapping.abort(),
-        ["<C-l>"] = cmp.mapping(function(fallback)    -- Move choice forward
-            if ls.choice_active() then
-                ls.change_choice()
-            else
-                fallback()
-            end
-        end),
-        ["<C-h>"] = cmp.mapping(function(fallback) -- Move chocie backward
-            if ls.choice_active() then
-                ls.change_choice(-1)
-            else
-                fallback()
-            end
-        end),
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    -- Expands snippets
-    snippet = {
-        expand = function(args)
-            ls.lsp_expand(args.body)
-        end,
-    },
+	sources = {
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
+		{ name = "path" },
+		{ name = "luasnip", option = { show_autosnippets = true } },
+	},
+	mapping = {
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif ls.expand_or_jumpable() then
+				ls.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }), -- Tab autocomplete
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif ls.jumpable(-1) then
+				ls.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Enter to complete
+		["<Up>"] = cmp.mapping.abort(), -- No up and down selection
+		["<Down>"] = cmp.mapping.abort(),
+		["<C-l>"] = cmp.mapping(function(fallback) -- Move choice forward
+			if ls.choice_active() then
+				ls.change_choice()
+			else
+				fallback()
+			end
+		end),
+		["<C-h>"] = cmp.mapping(function(fallback) -- Move chocie backward
+			if ls.choice_active() then
+				ls.change_choice(-1)
+			else
+				fallback()
+			end
+		end),
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	-- Expands snippets
+	snippet = {
+		expand = function(args)
+			ls.lsp_expand(args.body)
+		end,
+	},
 })
 
 -- Autoclose parenthesis on function completion
