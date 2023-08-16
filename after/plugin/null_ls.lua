1c9d06c5b2252e93f97d1ec1a04b89e9d8ab1503
Matteo Golin <matteo.golin@gmail.com>
2023-08-16 14:43:45 -0400

    Allow collapsing simple logigcal statements in stylua.

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -4,22 +4,22 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-	formatting.black.with({ extra_args = { "--line-length=120" } }),
-	formatting.prettierd.with({
-		filetypes = { "html", "css", "json", "markdown", "javascriptreact", "javascript" },
-	}),
-	formatting.stylua.with({extra_args = {"--indent_type=Spaces"}}),
-	formatting.clang_format.with({
-		filetypes = { "c", "c++" },
-		extra_args = { "-style={IndentWidth: 4, ColumnLimit: 120, AllowShortIfStatementsOnASingleLine: true}" },
-	}),
-	formatting.rustfmt,
-	diagnostics.flake8.with({ extra_args = { "--max-line-length", "120", "--ignore=E203" } }),
-	diagnostics.eslint,
-	code_actions.gitsigns,
+    formatting.black.with({ extra_args = { "--line-length=120" } }),
+    formatting.prettierd.with({
+        filetypes = { "html", "css", "json", "markdown", "javascriptreact", "javascript" },
+    }),
+    formatting.stylua.with({ extra_args = { "--indent-type=Spaces", "--collapse-simple-statement=Always" } }),
+    formatting.clang_format.with({
+        filetypes = { "c", "c++" },
+        extra_args = { "-style={IndentWidth: 4, ColumnLimit: 120, AllowShortIfStatementsOnASingleLine: true}" },
+    }),
+    formatting.rustfmt,
+    diagnostics.flake8.with({ extra_args = { "--max-line-length", "120", "--ignore=E203" } }),
+    diagnostics.eslint,
+    code_actions.gitsigns,
 }
 
 null_ls.setup({
-	debug = true,
-	sources = sources,
+    debug = true,
+    sources = sources,
 })
--- luasnippets/python.lua
+++ luasnippets/python.lua
@@ -1,47 +1,43 @@
 -- Regular snippets
 return {
-	s({ trig = "main", name = "Main gaurd.", dscr = "Python __main__ gaurd." }, {
-		t({ 'if __name__ == "__main__":', "\t" }),
-		c(1, {
-			t(""),
-			t("main()"),
-		}),
-	}),
+    s({ trig = "main", name = "Main gaurd.", dscr = "Python __main__ gaurd." }, {
+        t({ 'if __name__ == "__main__":', "\t" }),
+        c(1, {
+            t(""),
+            t("main()"),
+        }),
+    }),
 },
 -- Autosnippets
 {
-	s({ trig = "!author", dscr = "Set __author__ to my name." }, { t('__author__ = "Matteo Golin"') }),
-	s({ trig = "def", dscr = "Create a function/method definition with standard parameters." }, {
-		t({ "def " }),
-		i(1, "fn_name"),
-		d(2, function(args)
-			local in_class = false
-			local decorator_node = nil
-			local node = vim.treesitter.get_node()
+    s({ trig = "!author", dscr = "Set __author__ to my name." }, { t('__author__ = "Matteo Golin"') }),
+    s({ trig = "def", dscr = "Create a function/method definition with standard parameters." }, {
+        t({ "def " }),
+        i(1, "fn_name"),
+        d(2, function(args)
+            local in_class = false
+            local decorator_node = nil
+            local node = vim.treesitter.get_node()
 
-			-- Move up the tree until a class node is found
-			while node ~= nil do
-				in_class = node:type() == "class_definition"
-				if in_class then
-					break
-				end
-				node = node:parent()
-			end
+            -- Move up the tree until a class node is found
+            while node ~= nil do
+                in_class = node:type() == "class_definition"
+                if in_class then break end
+                node = node:parent()
+            end
 
-			-- Decide default args based on nodes
-			local arguments = "("
-			if in_class then
-				arguments = arguments .. "self, "
-			end
+            -- Decide default args based on nodes
+            local arguments = "("
+            if in_class then arguments = arguments .. "self, " end
 
-			return sn(nil, {
-				t({ arguments }),
-				i(1, "params"),
-				t({ ") -> " }),
-			})
-		end),
-		i(3, "None"),
-		t({ ":", "\t" }),
-		i(0, "pass"),
-	}),
+            return sn(nil, {
+                t({ arguments }),
+                i(1, "params"),
+                t({ ") -> " }),
+            })
+        end),
+        i(3, "None"),
+        t({ ":", "\t" }),
+        i(0, "pass"),
+    }),
 }
