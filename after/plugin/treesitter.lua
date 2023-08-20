93eef3f0263cf0a0c083619b5a6653935ac87fa5
Matteo Golin <matteo.golin@gmail.com>
2023-08-19 22:32:22 -0400

    Fixed small issue with f-strings.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,27 +1,27 @@
 require("nvim-treesitter.install").compilers = { "gcc" }
 require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		"c",
-		"lua",
-		"vim",
-		"vimdoc",
-		"query",
-		"python",
-		"cpp",
-		"rust",
-		"javascript",
-		"css",
-		"html",
-		"json",
-		"markdown",
-		"comment",
-	},
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+    ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "python",
+        "cpp",
+        "rust",
+        "javascript",
+        "css",
+        "html",
+        "json",
+        "markdown",
+        "comment",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
 })
 
 -- Custom TO DO highlight
@@ -30,36 +30,28 @@ vim.api.nvim_set_hl(0, "@text.todo", { link = "GruvboxRed" })
 -- Treesitter auto-format strings
 vim.api.nvim_create_augroup("py-fstring", { clear = true })
 vim.api.nvim_create_autocmd("InsertCharPre", {
-	pattern = { "*.py" },
-	group = "py-fstring",
-	callback = function(opts)
-		-- Only run if f-string escape character is typed
-		if vim.v.char ~= "{" then
-			return
-		end
+    pattern = { "*.py" },
+    group = "py-fstring",
+    callback = function(opts)
+        -- Only run if f-string escape character is typed
+        if vim.v.char ~= "{" then return end
 
-		-- Get node and return early if not in a string
-		local node = vim.treesitter.get_node()
+        -- Get node and return early if not in a string
+        local node = vim.treesitter.get_node()
 
-		if node:type() ~= "string" then
-			node = node:parent()
-		end
+        if node:type() ~= "string" then node = node:parent() end
+        if node:type() ~= "string" then return end
 
-		if node:type() ~= "string" then
-			return
-		end
+        vim.print(node:type())
+        local row, col, _, _ = vim.treesitter.get_node_range(node)
 
-		-- Get parent string node and its range
-		local row, col, _, _ = vim.treesitter.get_node_range(node:parent())
-		col = col + 1
+        -- Return early if string is already a format string
+        local first_char = vim.api.nvim_buf_get_text(opts.buf, row, col, row, col + 1, {})[1]
+        vim.print("row " .. row .. " col " .. col)
+        vim.print("char: '" .. first_char .. "'")
+        if first_char == "f" then return end
 
-		-- Return early if string is already a format string
-		local first_char = vim.api.nvim_buf_get_text(opts.buf, row, col, row, col + 1, {})[1]
-		if first_char == "f" then
-			return
-		end
-
-		-- Otherwise, make the string a format string
-		vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .. "|if<Esc>`'la")
-	end,
+        -- Otherwise, make the string a format string
+        vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .."|if<Esc>`'la")
+    end,
 })
