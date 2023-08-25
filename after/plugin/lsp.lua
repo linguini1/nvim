6532fa2eebf07932513b9f6eb6da6a0f3666cbc7
Matteo Golin <matteo.golin@gmail.com>
2023-08-25 09:51:00 -0400

    Added a bunch of default mappings.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,12 +8,31 @@ local lsnip = require("luasnip")
 
 -- LSP attach
 local function on_attach()
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "Show documentation in hover window." })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0, desc = "Jump to definition." })
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0, desc = "Jump to declaration." })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0, desc = "Jump to implementation." })
+    vim.keymap.set("n", "go", vim.lsp.buf.type_definition, { buffer = 0, desc = "Jump to type definition." })
+    vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, { buffer = 0, desc = "Jump to signature help." })
+    vim.keymap.set("n", "gq", vim.lsp.buf.format, { buffer = 0, desc = "Jump to signature help." })
     vim.keymap.set(
         "n",
         "gr",
-        "<Cmd>Telescope lsp_references<CR>",
+        require("telescope.builtin").lsp_references,
         { buffer = 0, desc = "Show references in a Telescope window." }
     )
+
+    -- Diagnostics
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = 0, desc = "Jump to next diagnostic." })
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = 0, desc = "Jump to previous diagnostic." })
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = 0, desc = "Show diagnostic information in hover." })
+
+    -- Code actions
+    if vim.lsp.buf.range_code_action then
+        vim.keymap.set("x", "<F4>", vim.lsp.buf.range_code_action, { buffer = 0, desc = "Range code action." })
+    else
+        vim.keymap.set("x", "<F4>", vim.lsp.buf.code_action, { buffer = 0, desc = "Code action." })
+    end
 end
 
 -- Mason
