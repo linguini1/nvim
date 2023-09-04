7031869c4a8d460659937bd272c75340a91db354
Matteo Golin <matteo.golin@gmail.com>
2023-09-04 17:42:40 -0400

    Created new plugins directory according to spec.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
+return {
     { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
     {
         "ellisonleao/gruvbox.nvim",
@@ -59,4 +46,4 @@ require("lazy").setup({
         end,
         ft = { "markdown" },
     },
-})
+}
