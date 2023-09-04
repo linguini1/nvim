a86f931d17abdfab43c4313684e6cecc3594d785
Matteo Golin <matteo.golin@gmail.com>
2023-09-04 19:21:14 -0400

    Removed reference to packer plugin count and replaced with lazy stats.

--- lua/startup/themes/linguini.lua
+++ lua/startup/themes/linguini.lua
@@ -1,51 +1,53 @@
 local settings = {
-	header = {
-		type = "text",
-		align = "center",
-		fold_section = false,
-		title = "Header",
-		margin = 3,
-		content = require("startup.custom_headers").conway,
-		highlight = "@variable",
-		oldfiles_amount = 0,
-	},
-	clock = {
-		type = "text",
-		content = function()
-			local time = "" .. os.date("%H:%M")
-			local date = "" .. os.date("%Y-%m-%d")
-			return { date, time }
-		end,
-		oldfiles_directory = false,
-		align = "center",
-		fold_section = false,
-		title = "",
-		margin = 3,
-		highlight = "@variable",
-	},
-	statistics = {
-		type = "text",
-		content = function()
-			local plugin_count = vim.tbl_count(packer_plugins)
-			local nvim_vers = string.match(vim.api.nvim_cmd({ cmd = "version" }, { output = true }), "NVIM v%d%.%d%.%d")
-			local os = "OS: " .. vim.loop.os_uname().sysname
-			return {
-				nvim_vers,
-				os,
-				string.format("Total plugins: %d", plugin_count),
-			}
-		end,
-		oldfiles_directory = false,
-		align = "center",
-		fold_section = false,
-		title = "",
-		margin = 3,
-		highlight = "@variable",
-	},
-	parts = {
-		"header",
-		"statistics",
-		"clock",
-	},
+    header = {
+        type = "text",
+        align = "center",
+        fold_section = false,
+        title = "Header",
+        margin = 3,
+        content = require("startup.custom_headers").conway,
+        highlight = "@variable",
+        oldfiles_amount = 0,
+    },
+    clock = {
+        type = "text",
+        content = function()
+            local time = "" .. os.date("%H:%M")
+            local date = "" .. os.date("%Y-%m-%d")
+            return { date, time }
+        end,
+        oldfiles_directory = false,
+        align = "center",
+        fold_section = false,
+        title = "",
+        margin = 3,
+        highlight = "@variable",
+    },
+    statistics = {
+        type = "text",
+        content = function()
+            local lazy_stats = require("lazy").stats()
+            local nvim_vers = string.match(vim.api.nvim_cmd({ cmd = "version" }, { output = true }), "NVIM v%d%.%d%.%d")
+            local os = "OS: " .. vim.loop.os_uname().sysname
+            return {
+                nvim_vers,
+                os,
+                string.format("Total plugins: %d", lazy_stats.count),
+                string.format("Plugins loaded: %d", lazy_stats.loaded),
+                string.format("Startup time: %fms", lazy_stats.startuptime),
+            }
+        end,
+        oldfiles_directory = false,
+        align = "center",
+        fold_section = false,
+        title = "",
+        margin = 3,
+        highlight = "@variable",
+    },
+    parts = {
+        "header",
+        "statistics",
+        "clock",
+    },
 }
 return settings
