762704e7baa9ff9e7e8e26658843b301c02ed405
linguini1 <matteo.golin@gmail.com>
2023-09-09 17:20:30 -0400

    Changed treesitter build sequence to actually update silently.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        build = function() require("nvim-treesitter.install").update({ with_sync = true })() end,
+        build = function() vim.cmd("silent! TSUpdate") end,
         config = function()
             require("nvim-treesitter.install").compilers = { "gcc" }
             require("nvim-treesitter.configs").setup({
