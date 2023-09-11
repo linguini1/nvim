2f26bf84623689f3226c261b55a50b8aa984881a
linguini1 <matteo.golin@gmail.com>
2023-09-11 13:10:03 -0400

    Created a snippet to define a C main function with either void or standard args.

--- luasnippets/c.lua
+++ luasnippets/c.lua
@@ -1,5 +1,16 @@
 -- Regular snippets
-return {},
+return {
+    s({ trig = "main", dscr = "Main function definition." }, {
+        t("int main("),
+        c(1, {
+            t("void"),
+            t("int argc, char **argv"),
+        }),
+        t({ ") {", "\t" }),
+        i(0),
+        t({ "", "\treturn 0;", "}" }),
+    }),
+},
 -- Autosnippets
 {
     s({ trig = "/*", dscr = "Multi-line comment." }, { t("/* "), i(1, "comment"), t(" */") }),
