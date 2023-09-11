1e9312a8c17cd6392f694140610640737f5cd207
linguini1 <matteo.golin@gmail.com>
2023-09-11 14:15:07 -0400

    New options format.

--- lua/plugins/pulse.lua
+++ lua/plugins/pulse.lua
@@ -6,8 +6,20 @@ return {
         pulse.setup({ level = vim.log.levels.INFO })
 
         -- Timers
-        pulse.add("eyes", 30, "Rest your eyes!", true)
-        pulse.add("drink", 20, "Drink some water!", true)
-        pulse.add("brain", 50, "Rest your mind.", true)
+        pulse.add("eyes", {
+            interval = 30,
+            message = "Rest your eyes!",
+            enabled = true,
+        })
+        pulse.add("drink", {
+            interval = 20,
+            message = "Drink some water!",
+            enabled = true,
+        })
+        pulse.add("brain", {
+            interval = 50,
+            message = "Rest your mind.",
+            enabled = true,
+        })
     end,
 }
