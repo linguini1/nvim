b886bdb8a0b8256b1435752dd6702b59e309befd
linguini1 <matteo.golin@gmail.com>
2023-09-15 17:10:53 -0400

    Fixed latex compiler name.

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,6 +2,6 @@ return {
     "lervag/vimtex",
     config = function ()
         vim.cmd("filetype plugin indent on")
-        vim.g.vimtex_compiler_method = "pdlatex"
+        vim.g.vimtex_compiler_method = "pdflatex"
     end
 }
