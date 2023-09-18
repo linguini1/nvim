a7196d8a30298c226277347122be2568de48cfb2
linguini1 <matteo.golin@gmail.com>
2023-09-17 21:11:33 -0400

    More LaTeX support.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,9 @@ return {
                     "^venv/",
                     "^./.git/",
                     ".idea",
+                    "*.synctex.gz",
+                    "*.fls",
+                    "*.fdb_latexmk",
                 },
             },
         })
