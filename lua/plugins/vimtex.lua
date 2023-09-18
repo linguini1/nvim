5fd490a58747c5d08583c2795c828cb0e5883184
Matteo Golin <matteo.golin@gmail.com>
2023-09-17 21:12:42 -0400

    Updated filetypes for VimTex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,8 +1,8 @@
 return {
     "lervag/vimtex",
-    ft = "latex",
-    config = function ()
+    ft = { "latex", "tex" },
+    config = function()
         vim.cmd("filetype plugin indent on")
-        vim.g.vimtex_compiler_method = "pdlatex"
-    end
+        vim.g.vimtex_compiler_method = "pdflatex"
+    end,
 }
