bd941a4f19fc6d467f7cbe3db5207c1091200b87
Matteo Golin <matteo.golin@gmail.com>
2023-09-17 21:41:04 -0400

    Added some more custom snippets.

--- luasnippets/latex.lua
+++
@@ -1,9 +0,0 @@
-return {}, {
-    s({ trig = "\\frac", name = "Fraction.", dscr = "LaTeX fraction." }, {
-        t("\\frac{"),
-        i(1, ""),
-        t("}{"),
-        i(0, ""),
-        t("}"),
-    }),
-}
