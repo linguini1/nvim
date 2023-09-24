82ce4fe67e31c656a9344d750d5e9c9d024d4593
linguini1 <matteo.golin@gmail.com>
2023-09-24 12:50:37 -0400

    Removed quickfix for vimtex cause it was annoying.

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,5 +4,6 @@ return {
     config = function()
         vim.cmd("filetype plugin indent on")
         vim.g.vimtex_compiler_method = "pdflatex"
+        vim.g.vimtex_quickfix_enabled = 0
     end,
 }
