4d4cf1567669def2ac0097176aea47cadb341ca5
linguini1 <matteo.golin@gmail.com>
2023-10-13 13:17:48 -0400

    Added snippet for creating ifndef headers.

--- luasnippets/cpp.lua
+++ luasnippets/cpp.lua
@@ -1,5 +1,13 @@
 -- Regular snippets
-return {},
+return {
+    s({ trig = "ifndef", dscr = "Create an ifndef header file guard." }, {
+        t("#ifndef "),
+        i(1),
+        f(function(args) return { "", "#define " .. args[1][1], "" } end, { 1 }),
+        i(0),
+        f(function(args) return { "", "#endif // " .. args[1][1] } end, { 1 }),
+    }),
+},
 -- Autosnippets
 {
     s({ trig = "/*", dscr = "Multi-line comment." }, { t("/* "), i(1, "comment"), t(" */") }),
