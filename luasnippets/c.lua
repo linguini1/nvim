09d0010074a829479cf355348e7877710a448401
linguini1 <matteo.golin@gmail.com>
2023-10-13 13:17:48 -0400

    Added snippet for creating ifndef headers.

--- luasnippets/c.lua
+++ luasnippets/c.lua
@@ -10,6 +10,13 @@ return {
         i(0),
         t({ "", "\treturn 0;", "}" }),
     }),
+    s({ trig = "ifndef", dscr = "Create an ifndef header file guard." }, {
+        t("ifndef "),
+        i(1),
+        f(function(args) return { "", "#define " .. args[1][1], "" } end, { 1 }),
+        i(0),
+        f(function(args) return { "", "#endif // " .. args[1][1] } end, { 1 }),
+    }),
 },
 -- Autosnippets
 {
