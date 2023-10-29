5c516693cfd968754656a3c87c617aaf269c4053
Matteo Golin <matteo.golin@gmail.com>
2023-10-29 14:57:08 -0400

    Added a command to modify compile_commands.json for QNX development.

--- lua/automation/editing/init.lua
+++ lua/automation/editing/init.lua
@@ -1,2 +1,3 @@
 require("automation.editing.hex-edit")
 require("automation.editing.fstring")
+require("automation.editing.qnxify")
---
+++ lua/automation/editing/qnxify.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_create_user_command("QNXify", function(_)
+    vim.cmd("%s/qcc/aarch64-unknown-nto-qnx7.1.0-gcc/g")
+    vim.cmd('%s/"-DNDEBUG",/"-DNDEBUG",\r\t"-D__QNXNTO__",\r\t"-D__LITTLEENDIAN__",')
+    vim.cmd('%s/"-V.*$/')
+end, { desc = "Modify compile_commands.json to work with qcc." })
