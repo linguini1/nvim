73e4f79501b01c9976cb1984c71d48be457b88bc
Matteo Golin <matteo.golin@gmail.com>
2023-12-09 09:59:46 -0500

    Use Zathura PDF viewer on Linux.

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,9 @@ return {
     ft = { "latex", "tex" },
     config = function()
         vim.cmd("filetype plugin indent on")
+        if vim.loop.os_uname().sysname == "Linux" then vim.g.vimtex_view_method = "zathura" end
         vim.g.vimtex_compiler_method = "pdflatex"
         vim.g.vimtex_quickfix_enabled = 0
+        vim.g.vimtex_view_forward_search_on_start = 0
     end,
 }
