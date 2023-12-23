a88f4ee787321c73ac08eaa988491037e89760f7
Matteo Golin <matteo.golin@gmail.com>
2023-12-23 11:58:46 -0500

    Updated colour scheme because the new Neovim default colour scheme broke a few things.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,7 +2,15 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-        require("gruvbox").setup({ contrast = "hard" })
+        require("gruvbox").setup({
+            contrast = "hard",
+            overrides = {
+                ["@punctuation.bracket"] = { link = "Special" },
+                ["@punctuation.delimiter"] = { link = "Special" },
+                ["@punctuation.special"] = { link = "Special" },
+            },
+        })
         vim.cmd.colorscheme("gruvbox")
+        vim.api.nvim_set_hl(0, "FloatBoarder", { link = "Normal" })
     end,
 }
