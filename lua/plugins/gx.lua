3fb0897fc1f4e313676756515c9fd13182cc28e8
Matteo Golin <matteo.golin@gmail.com>
2024-04-24 11:54:10 -0400

    Fixed gx.nvim keys.

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -1,6 +1,6 @@
 return {
     "chrishrb/gx.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    keys = { "gx" },
-    config = function() require("gx").setup({ handler_options = { search_engine = "google" } }) end,
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    config = function() require("gx").setup({ handler_options = { search_engine = "duckduckgo" } }) end,
 }
