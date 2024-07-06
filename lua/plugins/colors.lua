cd06b5b82d15b1d012114dae9e84141161c12f1f
Matteo Golin <matteo.golin@gmail.com>
2024-07-06 10:11:56 -0400

    Colour overrides no longer necessary.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -4,13 +4,6 @@ return {
     config = function()
         require("gruvbox").setup({
             contrast = "hard",
-            overrides = {
-                ["@punctuation.special"] = { link = "Special" },
-                ["@punctuation.delimiter"] = { link = "Special" },
-                ["@punctuation.bracket"] = { link = "Special" },
-                ["@punctuation"] = { link = "Special" },
-                ["Delimiter"] = { link = "Special" },
-            },
         })
         vim.cmd.colorscheme("gruvbox")
         vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
