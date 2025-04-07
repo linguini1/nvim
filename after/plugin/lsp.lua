d917332b20956d90c13017423dd9ffa266b126fa
Matteo Golin <matteo.golin@gmail.com>
2025-04-06 22:07:17 -0400

    Override window border style

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -217,9 +217,11 @@ vim.diagnostic.config({
     update_in_insert = true,
     underline = true,
 })
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-})
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-})
+
+-- Override border styling for hover window globally
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or "rounded"
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
