08f90150df488adeb4dd94671415497c51216369
Matteo Golin <matteo.golin@gmail.com>
2025-06-03 20:13:21 -0400

    Fix typo in figure snippet.

--- luasnippets/html.lua
+++ luasnippets/html.lua
@@ -1,5 +1,5 @@
 local blog_figure = s({ trig = "figure", dscr = "Figure element for my blog" }, {
-    unpack(fmt('<div class="figure">\n\t<img src="{}" alt="{}" size="{}">\n\t<p>{}</p>\n</div>', {
+    unpack(fmt('<div class="figure">\n\t<img src="{}" alt="{}" width="{}">\n\t<p>{}</p>\n</div>', {
         i(1, "link"),
         i(2, "alt-text"),
         i(3, "80%"),
