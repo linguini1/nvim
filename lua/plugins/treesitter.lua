1c6328094a7f6070015cc5085d875e1205f7d1be
Matteo Golin <matteo.golin@gmail.com>
2026-04-10 16:10:08 -0400

    Update to latest version of treesitter to resolve errors.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,46 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
         build = function() vim.cmd("silent! TSUpdate") end,
         config = function()
-            require("nvim-treesitter.install").compilers = { "clang", "gcc" }
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "lua",
-                    "c",
-                    "cpp",
-                    "make",
-                    "python",
-                    "vim",
-                    "vimdoc",
-                    "query",
-                    "rust",
-                    "javascript",
-                    "css",
-                    "html",
-                    "json",
-                    "markdown",
-                    "markdown_inline",
-                    "comment",
-                    "latex",
-                    "bash",
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true },
-                playground = { enable = true },
+            local ts = require("nvim-treesitter")
+            ts.setup({})
+            ts.install({
+                "lua",
+                "c",
+                "cpp",
+                "make",
+                "python",
+                "vim",
+                "vimdoc",
+                "query",
+                "rust",
+                "javascript",
+                "css",
+                "html",
+                "json",
+                "markdown",
+                "markdown_inline",
+                "comment",
+                "latex",
+                "bash",
             })
+
+            -- Enable treesitter highlighting on all files
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "*",
+                callback = function()
+                    local ft = vim.bo.filetype
+                    if ft and ft ~= "" then pcall(vim.treesitter.start) end
+                end,
+            })
+
+            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
             -- Custom TO DO highlight
             vim.api.nvim_set_hl(0, "@text.todo", { link = "GruvboxRed" })
         end,
     },
-    { "nvim-treesitter/playground" },
 }
