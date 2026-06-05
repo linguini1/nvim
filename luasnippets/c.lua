10d2700a62931e1c1e6843f34af3dac10c48d2e1
Matteo Golin <matteo.golin@gmail.com>
2026-06-05 14:22:40 -0400

    feat: added NuttX comment headers

--- luasnippets/c.lua
+++ luasnippets/c.lua
@@ -1,29 +1,49 @@
-local nuttx = require("luasnippets.nuttx");
+local regular_snippets = {}
 
-local ifndef_block = s({ trig = "ifndef", dscr = "Create an ifndef header file guard." }, {
-    t("#ifndef "),
-    i(1),
-    f(function(args) return { "", "#define " .. args[1][1], "" } end, { 1 }),
-    i(0),
-    f(function(args) return { "", "#endif // " .. args[1][1] } end, { 1 }),
-})
+-- Regular C snippets
 
-local javadoc_comment = s(
-    { trig = "/**", dscr = "Multi-line javadoc comment." },
-    { t("/** "), i(1, "comment"), t(" */") }
+table.insert(
+    regular_snippets,
+    s({ trig = "ifndef", dscr = "Create an ifndef header file guard." }, {
+        t("#ifndef "),
+        i(1),
+        f(
+            function(args) return { "", "#define " .. args[1][1], "" } end,
+            { 1 }
+        ),
+        i(0),
+        f(function(args) return { "", "#endif // " .. args[1][1] } end, { 1 }),
+    })
 )
 
-local multiline_comment = s({ trig = "/*", dscr = "Multi-line comment." }, { t("/* "), i(1, "comment"), t(" */") })
+table.insert(
+    regular_snippets,
+    s(
+        { trig = "/**", dscr = "Multi-line javadoc comment." },
+        { t("/** "), i(1, "comment"), t(" */") }
+    )
+)
 
-local main_func = s({ trig = "main", dscr = "Main function definition." }, {
-    unpack(fmt("int main({}) {{\n\t{}\n\treturn 0;\n}}", {
-        c(1, {
-            t("void"),
-            t("int argc, char **argv"),
-        }),
-        i(0),
-    })),
-})
+table.insert(
+    regular_snippets,
+    s(
+        { trig = "/*", dscr = "Multi-line comment." },
+        { t("/* "), i(1, "comment"), t(" */") }
+    )
+)
+
+table.insert(
+    regular_snippets,
+    s({ trig = "main", dscr = "Main function definition." }, {
+        unpack(fmt("int main({}) {{\n\t{}\n\treturn 0;\n}}", {
+            c(1, {
+                t("void"),
+                t("int argc, char **argv"),
+            }),
+            i(0),
+        })),
+    })
+)
 
 local arr_size = s({ trig = "!arr_size", dscr = "Array size macro" }, {
     t("#define arr_size(arr) (sizeof(arr)/sizeof(arr[0]))"),
@@ -33,15 +53,46 @@ local deref = s({ trig = "!deref", dscr = "Pointer dereferencing macro" }, {
     t("#define deref(type, p) (*((type*)(p)))"),
 })
 
--- Regular snippets
-return {
-    ifndef_block,
-    multiline_comment,
-    main_func,
-},
--- Autosnippets
-{
-    javadoc_comment,
-    arr_size,
-    deref,
+-- NuttX specific snippets
+
+-- Comment headers for sections
+local COMMENT_HEADERS = {
+    "Included Files",
+    "Pre-processor Definitions",
+    "Private Types",
+    "Public Types",
+    "Private Data",
+    "Public Data",
+    "Private Function Prototypes",
+    "Public Function Prototypes",
+    "Private Functions",
+    "Public Functions",
 }
+
+local function comment_header(text)
+    return t({
+        "/****************************************************************************",
+        "* " .. text,
+        "****************************************************************************/",
+    })
+end
+
+for _, header in ipairs(COMMENT_HEADERS) do
+    table.insert(
+        regular_snippets,
+        s({
+            trig = header,
+            dscr = header .. " Comment Header",
+        }, {
+            comment_header(header),
+        })
+    )
+end
+
+return regular_snippets,
+    -- Autosnippets
+    {
+        javadoc_comment,
+        arr_size,
+        deref,
+    }
