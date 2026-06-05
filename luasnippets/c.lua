14931b3d942cf103540dcf0eaa49ebce4145b744
Matteo Golin <matteo.golin@gmail.com>
2026-06-05 15:02:58 -0400

    feat: added NuttX-style file headers to snippets

--- luasnippets/c.lua
+++ luasnippets/c.lua
@@ -89,6 +89,62 @@ for _, header in ipairs(COMMENT_HEADERS) do
     )
 end
 
+table.insert(
+    regular_snippets,
+    s({
+        trig = "nxheader",
+        dscr = "NuttX top-file comment header with SPDX info",
+    }, {
+        t({
+            "/****************************************************************************",
+            "* ",
+        }),
+        -- Get the workdir-relative file name for the top
+        f(function(_) return vim.fn.expand("%") end),
+        t({
+            "",
+            "*",
+            "* SPDX-License-Identifer: ",
+        }),
+        -- License choice
+        c(1, {
+            t("Apache-2.0"),
+            i(0, "Other license"),
+        }),
+        t({ "", "*", "" }),
+        -- Copyright text
+        d(2, function(args)
+            if args[1][1] == "Apache-2.0" then
+                return sn(nil, {
+                    t({
+                        [[* Licensed to the Apache Software Foundation (ASF) under one or more]],
+                        [[* contributor license agreements. See the NOTICE file distributed with]],
+                        [[* this work for additional information regarding copyright ownership. The]],
+                        [[* ASF licenses this file to you under the Apache License, Version 2.0 (the]],
+                        [[* "License"); you may not use this file except in compliance with the]],
+                        [[* License. You may obtain a copy of the License at]],
+                        [[*]],
+                        [[*   http://www.apache.org/licenses/LICENSE-2.0]],
+                        [[*]],
+                        [[* Unless required by applicable law or agreed to in writing, software]],
+                        [[* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT]],
+                        [[* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the]],
+                        [[* License for the specific language governing permissions and limitations]],
+                        [[* under the License.]],
+                    }),
+                })
+            else
+                return sn(nil, { t("* "), i(1, "Custom text") })
+            end
+        end, { 1 }),
+        t({
+            "",
+            "*",
+            "****************************************************************************/",
+        }),
+    })
+)
+
 return regular_snippets,
     -- Autosnippets
     {
